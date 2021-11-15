2296b869e168e999dc186822190e90685394a42b
Cat-Hanbit <gasida.seo@gmail.com>
2021-11-15 23:09:15 +0900

    cilium lab

--- cilium/master.sh
+++ cilium/master.sh
@@ -42,11 +42,19 @@ PS1='$(kube_ps1)'$PS1
 EOT
 kubectl config rename-context "kubernetes-admin@kubernetes" "cilium-k8s"
 
+## kube-tail install
+curl -O https://raw.githubusercontent.com/johanhaleby/kubetail/master/kubetail
+chmod 744 kubetail && mv kubetail /usr/bin
+curl -o /root/kubetail.bash https://raw.githubusercontent.com/johanhaleby/kubetail/master/completion/kubetail.bash
+cat <<EOT >> ~/.bash_profile
+source /root/kubetail.bash
+EOT
+
 # Install Helm v3
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 
 # Install Clilium w/Helm
 helm repo add cilium https://helm.cilium.io/
-helm install cilium cilium/cilium --version 1.10.5 --namespace kube-system --set kubeProxyReplacement=strict --set k8sServiceHost=192.168.200.10 --set k8sServicePort=6443 --set ipam.mode=cluster-pool --set ipam.operator.clusterPoolIPv4PodCIDR=172.20.0.0/16 --set ipam.operator.clusterPoolIPv4MaskSize=24
+helm install cilium cilium/cilium --version 1.10.5 --namespace kube-system --set kubeProxyReplacement=strict --set k8sServiceHost=192.168.200.10 --set k8sServicePort=6443 --set ipam.mode=cluster-pool --set ipam.operator.clusterPoolIPv4PodCIDR=172.16.0.0/16 --set ipam.operator.clusterPoolIPv4MaskSize=24
 curl -L --remote-name-all https://github.com/cilium/cilium-cli/releases/latest/download/cilium-linux-amd64.tar.gz
 tar xzvfC cilium-linux-amd64.tar.gz /usr/local/bin
